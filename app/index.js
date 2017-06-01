3ae34440e4a5b3e3bbed3c3c16e54e0f6cc4ea79
Matthew Dean <deanmatthew16@gmail.com>
2017-05-31 23:29:04 -0400

    Initial commit

---
+++ app/index.js
@@ -0,0 +1,8 @@
+import Vue from 'vue';
+
+import App from './test';
+
+new Vue({
+    el: '#app',
+    render: (h) => h(App)
+});
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    context: __dirname,
+    entry: {
+        app: './app/index.js'
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js',
+        publicPath: ''
+    },
+    resolve: {
+        extensions: ['.js', '.vue']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                use: {
+                    loader: 'vue-loader',
+                    options: { esModule: true }
+                }
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'app/index.html',
+            cache: true
+        }),
+    ]
+};
