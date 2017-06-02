9961b3c51a6014a39d7b78b377030c299ac86c3b
Matthew Dean <deanmatthew16@gmail.com>
2017-06-02 05:30:23 -0400

    Use a shorter date format

--- app/filters/date.filter.js
+++ app/filters/date.filter.js
@@ -2,5 +2,5 @@ import moment from 'moment';
 
 export default function(date) {
     if (date === undefined || date === null || (typeof date !== 'number' && !(date instanceof Date))) return '';
-    return moment(date).format('LLL');
+    return moment(date).format('MMM D YYYY');
 }
