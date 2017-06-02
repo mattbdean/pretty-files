47abb18dd2185239d2e2b134eccfff3258e3921a
Matthew Dean <deanmatthew16@gmail.com>
2017-06-02 04:47:22 -0400

    Add vue-devtools

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
 const url = require('url');
+const devtools = require('vue-devtools');
 
 let win;
 
@@ -12,13 +13,15 @@ const createWindow = () => {
     //     slashes: true
     // }));
 
+    devtools.install();
+
     // Connect to port hosted by webpack-dev-server for HMR
     win.loadURL('http://localhost:3000');
     win.webContents.openDevTools();
 
     win.on('closed', () => {
         win = null;
-    })
+    });
 };
 
 app.on('ready', createWindow);
